dede01b5f20d22af1364142ae851c66932ae3b06
Xaroz <33064781+Xaroz@users.noreply.github.com>
2018-07-10 17:08:04 -0400

    Profesor

---
+++ Tecnicas/Tecnicas/Profesor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnicas
+{
+    class Profesor
+    {
+        public int profesorId { get; set; }
+        public string profesorNombre { get; set; }
+        public string profesorApellido { get; set; }
+    }
+}
